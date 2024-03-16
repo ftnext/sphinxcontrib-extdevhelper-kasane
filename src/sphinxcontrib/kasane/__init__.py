433cfecbcf34cdeae7a4499053dda9f85fa309c5
ftnext <takuyafjp+develop@gmail.com>
2024-03-16 13:51:09 +0900

    [refactor] Extract conditions module

--- src/sphinxcontrib/kasane/__init__.py
+++ src/sphinxcontrib/kasane/__init__.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
 import types
-from typing import Protocol
 
 from sphinx.application import Sphinx
-from sphinx.builders import Builder
+
+from sphinxcontrib.kasane.conditions import BuilderCondition
 
 __version__ = "0.0.1"
 
@@ -36,15 +36,3 @@ class MixinDynamicInheritance:
         return types.new_class(
             self.new_class_name, (self.mixin_class, existing_class), {}
         )
-
-
-class BuilderCondition(Protocol):
-    def is_satisfied_by(self, builder: Builder) -> bool: ...
-
-
-class BuilderFormatCondition:
-    def __init__(self, format: str) -> None:
-        self.format = format
-
-    def is_satisfied_by(self, builder: Builder) -> bool:
-        return builder.format == self.format
--- tests/test_kasane.py
+++ tests/test_kasane.py
@@ -5,12 +5,8 @@ from sphinx.application import Sphinx
 from sphinx.builders import Builder
 from sphinx.registry import SphinxComponentRegistry
 
-from sphinxcontrib.kasane import (
-    BuilderCondition,
-    BuilderFormatCondition,
-    MixinDynamicInheritance,
-    TranslatorSetUp,
-)
+from sphinxcontrib.kasane import MixinDynamicInheritance, TranslatorSetUp
+from sphinxcontrib.kasane.conditions import BuilderCondition
 
 
 class TestTranslatorSetUp:
@@ -96,21 +92,3 @@ class TestMixinDynamicInheritance:
         assert actual.__name__ == "AwesomeNewClass"
         assert issubclass(actual, AwesomeMixin)
         assert issubclass(actual, SomeClass)
-
-
-class TestBuilderFormatCondition:
-    def test_satisfied(self):
-        html_format_builder = MagicMock(spec=Builder)
-        html_format_builder.format = "html"
-
-        sut = BuilderFormatCondition("html")
-
-        assert sut.is_satisfied_by(html_format_builder)
-
-    def test_not_satisfied(self):
-        not_html_format_builder = MagicMock(spec=Builder)
-        not_html_format_builder.format = "text"
-
-        sut = BuilderFormatCondition("html")
-
-        assert not sut.is_satisfied_by(not_html_format_builder)
