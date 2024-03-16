5a224a9e718d9278a911e9f204fcdc9769067f83
ftnext <takuyafjp+develop@gmail.com>
2024-03-16 10:09:16 +0900

    [feat] Set translator to Sphinx application

--- src/sphinxcontrib/kasane/__init__.py
+++ src/sphinxcontrib/kasane/__init__.py
@@ -1,8 +1,35 @@
-import types
+from __future__ import annotations
 
+import types
+from typing import Protocol
+
+from sphinx.application import Sphinx
 from sphinx.builders import Builder
 
 __version__ = "0.0.1"
+
+
+class TranslatorSetUp:
+    def __init__(
+        self, inheritance: MixinDynamicInheritance, condition: BuilderCondition
+    ) -> None:
+        self.inheritance = inheritance
+        self.condition = condition
+
+    def __call__(self, app: Sphinx) -> None:
+        if not self.condition.is_satisfied_by(app.builder):
+            return
+
+        builder_name = app.builder.name
+        if translator_class := app.registry.translators.get(builder_name):
+            app.set_translator(
+                builder_name, self.inheritance(translator_class), override=True
+            )
+        else:
+            app.set_translator(
+                builder_name,
+                self.inheritance(app.builder.default_translator_class),
+            )
 
 
 class MixinDynamicInheritance:
@@ -14,6 +41,10 @@ class MixinDynamicInheritance:
         return types.new_class(
             self.new_class_name, (self.mixin_class, existing_class), {}
         )
+
+
+class BuilderCondition(Protocol):
+    def is_satisfied_by(self, builder: Builder) -> bool: ...
 
 
 class BuilderFormatCondition:
